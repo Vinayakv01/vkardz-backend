e1629eabb4cd8c145f8e664a57c36aad2b33fdd0
AdityaRajaram <aditya.rajaram13@gmail.com>
2023-07-11 20:17:33 +0530

    setupsql

--- services/db.js
+++ services/db.js
@@ -4,6 +4,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER, 
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 });
 
 connection.connect((err) => {
